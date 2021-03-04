ea978cfd2946bca762e9792e1dfce26d5fef2acb
Turbo6230 <71839625+Turbo6230@users.noreply.github.com>
2021-03-04 07:40:52 -0500

    Tansitioning between screens

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //@IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    
+    @IBAction func yellowButton(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    
+    @IBAction func greenButton(_ sender: UIButton) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+    
+    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       // segue.destination.navigationItem.title = textField.text
+    //}
+    
 }
 
